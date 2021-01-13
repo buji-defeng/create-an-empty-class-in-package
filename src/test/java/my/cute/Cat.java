be0f821059207be3e9e5fee9ea2b2c57254d17e2
bujidefeng <1587770721@qq.com>
2021-01-13 11:24:56 +0800

    在指定的包中创建一个类

---
+++ src/test/java/my/cute/Cat.java
@@ -0,0 +1,13 @@
+package my.cute;
+/**
+ * @auther chenyuzhu
+ * @ClassName Cat
+ * @Description
+ * @Date 2021-01-13
+ */
+
+public class Cat {
+    public static void main(String[] args) {
+        System.out.println("cat");
+    }
+}
